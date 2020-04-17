ff06dbf262a4171e11b839d42b77e8056ccbf666
James Kirk <james.kirk84@gmail.com>
2020-04-17 19:06:10 +0100

    Fixing InstagramFeed

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -8,7 +8,7 @@ import './InstagramFeed.css'
 
 export default class InstagramFeed extends Component {
   static defaultProps = {
-    accessToken: `${process.env.INSTAGRAM_API_TOKEN}`,
+    accessToken: `${process.env.GATSBY_INSTAGRAM_API_TOKEN}`,
     count: 20
   }
 
