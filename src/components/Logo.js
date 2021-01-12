3263bec975f5293b5138b73569cf562c963d950c
James Kirk <james.kirk84@gmail.com>
2021-01-12 23:18:25 +0000

    Updating logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,6 +4,7 @@ import './Logo.css'
 export default () => (
   <div
     className="Logo"
+    aria-label="Liz Kirk Personal Training Logo"
     style={{
       backgroundImage: `url(/images/logo.png)`
     }}
